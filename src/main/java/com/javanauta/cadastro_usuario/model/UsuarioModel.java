dd03cc1cc025a374a9b22e894dc69092888fc873
Fabricio3916 <fabriciopassos3916@gmail.com>
2025-06-17 22:16:48 -0300

    feat: cria a camada de model

--- src/main/java/com/javanauta/cadastro_usuario/model/UsuarioModel.java
+++ src/main/java/com/javanauta/cadastro_usuario/model/UsuarioModel.java
@@ -1,4 +1,29 @@
 package com.javanauta.cadastro_usuario.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table
+@Entity(name = "usuario")
 public class UsuarioModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name= "email", unique = true)
+    private String email;
+
+    @Column(name = "nome")
+    private String nome;
+
+
+
 }

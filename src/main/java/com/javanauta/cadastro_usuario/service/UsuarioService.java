38b1fc7745d7f1df90017215b869004a86ad665a
Fabricio3916 <fabriciopassos3916@gmail.com>
2025-06-17 22:17:04 -0300

    feat: cria a camada de service

--- src/main/java/com/javanauta/cadastro_usuario/service/UsuarioService.java
+++ src/main/java/com/javanauta/cadastro_usuario/service/UsuarioService.java
@@ -1,4 +1,43 @@
 package com.javanauta.cadastro_usuario.service;
 
+import com.javanauta.cadastro_usuario.model.UsuarioModel;
+import com.javanauta.cadastro_usuario.repository.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UsuarioService {
+
+    private final UsuarioRepository repository;
+
+    public UsuarioService(UsuarioRepository repository) {
+        this.repository = repository;
+    }
+
+    public void salvarUsuario(UsuarioModel usuario){
+        repository.saveAndFlush(usuario);
+    }
+
+    public  UsuarioModel buscarUsuarioPorEmail(String email){
+        return repository.findByEmail(email).orElseThrow(
+                () -> new RuntimeException("Email não encontrado")
+        );
+    }
+
+    public void deletarUsuarioPorEmail(String email){
+        repository.deleteByEmail(email);
+    }
+
+    public void atualizarUsuarioPorId(Integer id, UsuarioModel usuario){
+        UsuarioModel usuarioEntity = repository.findById(id).orElseThrow(
+                () -> new RuntimeException("Usuario não encontrado")
+        );
+        UsuarioModel usuarioAtualizado = UsuarioModel.builder()
+                .email(usuario.getEmail() != null ?
+                        usuario.getEmail() : usuarioEntity.getEmail())
+                .nome(usuario.getNome() != null ?
+                        usuario.getNome() : usuarioEntity.getNome())
+                .id(usuarioEntity.getId())
+                .build();
+    }
+
 }
